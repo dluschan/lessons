20ff47608c7e501da49b7a1768a40067f575ab2a
Дмитрий Лущан <dluschan@gmail.com>
2017-01-21 09:56:42 +0300

    добавлен тест

---
+++ vector/vectorTest.pas
@@ -0,0 +1,29 @@
+program vectorTest;
+uses
+	vectorlib;
+var
+	m: Vector;
+	a, b: Iterator;
+	t: ^integer;
+
+begin
+	m := Vector.create();
+
+	m.push_back(112);
+	m.push_back(-8);
+	m.push_back(87);
+
+	a := m.start();
+	m.insert(a, 5);
+	b := m.finish();
+	m.insert(b, 50);
+
+	b := m.finish();
+	while not a.equal(b) do
+	begin
+		write(a.data(), ' ');
+		a := a.next();
+	end;
+	writeln();
+	writeln(m.at(2));
+end.
