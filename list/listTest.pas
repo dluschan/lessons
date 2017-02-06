c2e7bcc553cf614691a4a8ba6851a544b766c169
Дмитрий Лущан <dluschan@gmail.com>
2017-02-06 23:31:36 +0300

    расширен тест

--- list/listTest.pas
+++ list/listTest.pas
@@ -2,25 +2,48 @@ program listTest;
 uses
 	listlib;
 var
-	m: List;
-	a, b: Iterator;
+	m: list;
+	a, b: list_iterator;
 
 begin
-	m := List.create();
+	m := list.create();
+	m.push_back(1);
+	m.push_back(2);
+	m.push_back(3);
+	m.push_back(4);
+	m.push_back(5);
 
-	m.push_back(112);
-	m.push_back(-8);
-	m.push_back(87);
-
-	a := m.start();
-	m.insert(a, 37);
-
-	a := m.start();
-	b := m.finish();
+	a := m.get_begin();
+	a.next();
+	a.next();
+	m.insert(a, -1);
+	m.insert(a, -2);
+	m.insert(a, -3);
+	a.destroy();
+	
+	a := m.get_begin();
+	b := m.get_end();
 	while not a.equal(b) do
 	begin
-		write(a.data(), ' ');
-		a := a.next();
+		write(a.get_data(), ' ');
+		a.next();
+	end;
+	writeln();
+	
+	a.destroy();
+	b.destroy();
+
+	a := m.get_begin();
+	a.next();
+	m.erase(a);
+	m.erase(a);
+
+	a := m.get_begin();
+	b := m.get_end();
+	while not a.equal(b) do
+	begin
+		write(a.get_data(), ' ');
+		a.next();
 	end;
 	writeln();
 end.
