cd4e8b1f1f953bc1c4e257c01eef73b6c3bdc218
Дмитрий Лущан <dluschan@gmail.com>
2017-02-08 00:44:22 +0300

    тест проверяет операции push_back, pop_back, insert, erase

--- containers/pascal/tests/test1.pas
+++ containers/pascal/tests/test1.pas
@@ -1,10 +1,41 @@
-program test1;
+program test_push_pop_insert_erase;
 uses
 	containers;
-var
-	m: container;
-	a, b: iterator;
 
+function to_str(c: container): string;
+var
+	it_begin, it_end: iterator;
+	res, s: string;
+begin
+	res := '';
+	it_begin := c.get_begin();
+	it_end := c.get_end();
+	while not it_begin.equal(it_end) do
+	begin
+		str(it_begin.get_data():1, s);
+		res := res + s + ' ';
+		it_begin.next();
+	end;
+	it_begin.destroy();
+	it_end.destroy();
+	delete(res, length(res), 1);
+	to_str := res;
+end;
+
+procedure print(c: container);
+begin
+	writeln(to_str(c));
+end;
+
+function check(c: container; pattern: string): boolean;
+begin
+	check := to_str(c) = pattern;
+end;
+
+procedure test();
+var
+	m, m2: container;
+	it: iterator;
 begin
 	m := container.create();
 	m.push_back(1);
@@ -13,37 +44,49 @@ begin
 	m.push_back(4);
 	m.push_back(5);
 
-	a := m.get_begin();
-	a.next();
-	a.next();
-	m.insert(a, -1);
-	m.insert(a, -2);
-	m.insert(a, -3);
-	a.destroy();
-	
-	a := m.get_begin();
-	b := m.get_end();
-	while not a.equal(b) do
-	begin
-		write(a.get_data(), ' ');
-		a.next();
-	end;
-	writeln();
-	
-	a.destroy();
-	b.destroy();
+	if check(m, '1 2 3 4 5') then
+		writeln('Ok')
+	else
+		writeln('Foo');
 
-	a := m.get_begin();
-	a.next();
-	m.erase(a);
-	m.erase(a);
+	it := m.get_begin();
+	it.next();
+	it.next();
+	m.insert(it, -1);
+	m.insert(it, -2);
+	m.insert(it, -3);
+	it.destroy();
 
-	a := m.get_begin();
-	b := m.get_end();
-	while not a.equal(b) do
-	begin
-		write(a.get_data(), ' ');
-		a.next();
-	end;
-	writeln();
+	if check(m, '1 2 -3 -2 -1 3 4 5') then
+		writeln('Ok')
+	else
+		writeln('Foo');
+
+	it := m.get_begin();
+	it.next();
+	m.erase(it);
+	m.erase(it);
+	it.destroy();
+
+	if check(m, '1 -2 -1 3 4 5') then
+		writeln('Ok')
+	else
+		writeln('Foo');
+	
+	m2 := container.create();
+	while not m.empty() do
+		m2.push_back(m.pop_back());
+
+	m.destroy();
+	
+	if check(m2, '5 4 3 -1 -2 1') then
+		writeln('Ok')
+	else
+		writeln('Foo');
+
+	m2.destroy();
+end;
+
+begin
+	test();
 end.
