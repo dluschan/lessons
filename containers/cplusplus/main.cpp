2e89dd5c1b43bb00a28e4c1ad2dbb34d0f218d0c
Danila <manakov.danila@mail.ru>
2018-09-16 12:07:46 +0300

    Добавил vector.cpp

--- containers/cplusplus/main.cpp
+++ containers/cplusplus/main.cpp
@@ -1,82 +1,11 @@
 #include <algorithm>
 #include <iostream>
 #include "vector.h"
-#include "list.h"
-#include "map.h"
 
 using namespace std;
-using namespace containers;
-
-template<class T>
-void proc(T& v)
-{
-	for (int i = 0; i < 10; ++i)
-		v.push_back(rand() % 100);
-
-	for (auto x: v)
-		cout << x << ' ';
-	cout << endl;
-
-	sort(v.begin(), v.end());
-
-	for (auto x: v)
-		cout << x << ' ';
-	cout << endl;
-}
-
-void proc(list& l)
-{
-	for (int i = 0; i < 10; ++i)
-		l.push_back(rand() % 100);
-
-	auto it = l.begin();
-	auto itEnd = l.end();
-
-	while (it != itEnd)
-		cout << *(it++) << ' ';
-	cout << endl;
-
-	it = l.begin();
-
-	while (it++ != itEnd)
-		l.pop_back();
-
-	it = l.begin();
-
-	while (it != itEnd)
-		cout << *(it++) << ' ';
-	cout << endl;
-}
-
-void procHeavy(list& l, int n, int k)
-{
-	while (n--)
-	{
-		for (int i = 0; i < k; ++i)
-			l.push_back(rand() % 100);
-		for (int i = 0; i < k; ++i)
-			l.pop_back();
-	}
-}
-
-void proc(map& m)
-{
-	m[1] = 1;
-	m[2] = 2;
-	m[3] = 3;
-	auto it = m.begin();
-	auto itEnd = m.end();
-	for (; it != itEnd; ++it)
-		cout << *it << ' ';
-	cout << endl;
-}
 
 int main()
 {
-	vector v;
-	list l;
-	map m;
-	proc(m);
-	//procHeavy(l, 5, 1000);
+	containers::vector v;
 	return 0;
 }
---
+++ containers/cplusplus/vector.cpp
@@ -0,0 +1,78 @@
+#include "vector.h"
+
+namespace containers
+{
+    vector::vector(int n)
+	{
+		m_begin = new int[n];
+		m_end = m_begin;
+		m_capacity = m_begin + n;
+	}
+
+	int vector::operator[](int n)
+	{
+		return m_begin[n];
+	}
+
+	vector_iterator vector::begin()
+	{
+		return m_begin;
+	}
+	
+    vector_iterator vector::end()
+	{
+		return m_end;
+	}
+
+	void vector::push_back(int x)
+	{
+		if (m_capacity == m_end)
+		{
+			const int size = m_end - m_begin;
+
+			int new_size = 3 * size / 2 + 1;
+
+			int *new_begin = new int[new_size];
+	    	while(m_begin < m_end)
+				*new_begin++ = *m_begin++;
+			m_begin -= size;
+			new_begin -= size;
+			delete[] m_begin;
+			m_begin = new_begin;
+			m_end = m_begin + size;
+			m_capacity = m_begin + new_size;
+		}
+		*m_end++ = x;
+	}
+
+	int vector::pop_back()
+	{
+		return *--m_end;
+	}
+
+	void vector::insert(vector_iterator p, int x)
+	{
+		if (m_capacity == m_end)
+		{
+			const int size = m_begin == m_end;
+			int new_size = 3 * size / 2 + 1;
+			int *new_begin = new int[size];
+			while(m_begin < m_end)
+				*new_begin++ = *m_begin++;
+			delete[] m_begin;
+			m_begin = new_begin;
+			m_end = m_begin + size;
+			m_capacity = m_begin + new_size;
+		}
+		for (vector_iterator it = m_end; it > p; --it)
+			*it = *(it - 1);
+		*p = x;
+	}
+
+	void vector::erase(vector_iterator p)
+	{
+		for (vector_iterator it = p; it < m_end; ++it)
+			*it = *(it + 1);
+		--m_end;
+	}
+};
