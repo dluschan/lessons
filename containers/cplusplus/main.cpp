93671cf6a7891d0de84a2cec2e05c3261b433fc8
Danila <manakov.danila@mail.ru>
2018-09-17 21:55:52 +0300

    Пример vector::iterator

--- containers/cplusplus/main.cpp
+++ containers/cplusplus/main.cpp
@@ -20,13 +20,12 @@ int main()
 
 	cout<<"\n";
 
-	containers::vector::iterator it = v.begin();
 	//n+=v.insert(v.begin() + zero_pos, 9);
 	//std::cout<<v.begin()<<" "<<v.begin()+zero_pos<<"\n\n";
 	n-=v.erase(v.begin()+zero_pos);
 	n+=v.insert(v.begin()+zero_pos, 9);
-	for(int i=0; i<n; i++)
-		cout<<v[i]<<"\n";
+	for(containers::vector::iterator it = v.begin(); it < v.end(); it++)
+		cout<<*&it<<"\n";
 
 	return 0;
 }
